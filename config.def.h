b3784b2c081dff294ec71112ddb8a88ce2f1206f
Samuel D <samueld@mailo.com>
2022-03-24 12:16:16 +0100

    add systray

--- config.def.h
+++ config.def.h
@@ -12,6 +12,11 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const int user_bh            = 30;        /* 0 means that dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
 
+static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
+static const unsigned int systrayspacing = 2;   /* systray spacing */
+static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
+static const int showsystray             = 1;   /* 0 means no systray */
+
 static const unsigned int baralpha    = 0xc0;
 static const unsigned int borderalpha = OPAQUE;
 
